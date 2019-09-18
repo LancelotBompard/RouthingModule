35af3ffa99aae3e3a3c287a654ccf4dc7c43168a
LancelotBompard <lancelot.bompard@devinci.fr>
2019-09-18 18:37:10 +0200

    Angular Route Parameters

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  product = {
+    id: 100,
+    title: 'How to make router & navigation in Angular 8',
+    author: 'Didin J.',
+    description: 'A complete tutorial about creating router and navigation in the Angular 8 Web Application'
+  };
+
   constructor() { }
 
   ngOnInit() {
